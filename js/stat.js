7a5f67bc829404fdab32b105f0777b6af856d2fc
Stas <leshuksu@gmail.com>
2022-03-27 17:42:43 +0300

    Started bar graph generator (raw)

--- js/stat.js
+++ js/stat.js
@@ -16,6 +16,15 @@ var cloudShadow = {
   positionY: cloudBody.positionY + 10,
 };
 
+var playerBar = {
+  startPositionX: 155,
+  positionY: 100,
+  width: 40,
+  height: 150,
+  gap: 50,
+  defaultRed: 'rgba(255, 0, 0, 1)',
+};
+
 var renderCloud = function (ctx, renderObject, isStroke) {
   ctx.fillStyle = renderObject.color;
   ctx.fillRect(renderObject.positionX, renderObject.positionY, renderObject.width, renderObject.height);
@@ -33,4 +42,16 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 240, 30);
   ctx.fillText('Список результатов:', 230, 45);
+
+  var barPositionX = playerBar.startPositionX;
+
+  for (var i = 0; i < times.length; i++) {
+
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = playerBar.defaultRed;
+    }
+
+    ctx.fillRect(barPositionX, playerBar.positionY, playerBar.width, playerBar.height);
+    barPositionX += playerBar.width + playerBar.gap;
+  }
 };
