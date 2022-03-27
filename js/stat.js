fca6c4c6e991bc85863d8f5837230a9170c621c5
Stas <leshuksu@gmail.com>
2022-03-28 02:34:42 +0300

    module2-task1 is done, complete refactoring

--- js/stat.js
+++ js/stat.js
@@ -30,6 +30,14 @@ var playerBar = {
   },
 };
 
+var textStyle = {
+  font: '16px PT Mono',
+  fontSize: 16,
+  color: 'black',
+  defaultPositionX: 160,
+  defaultPositionY: 30,
+};
+
 var renderCloud = function (ctx, renderObject, isStroke) {
   ctx.fillStyle = renderObject.color;
   ctx.fillRect(renderObject.positionX, renderObject.positionY, renderObject.width, renderObject.height);
@@ -39,14 +47,22 @@ var renderCloud = function (ctx, renderObject, isStroke) {
   }
 };
 
+var renderTitleLine = function (ctx, titleText, renderObject, lineNumber) {
+  ctx.fillStyle = renderObject.color;
+  ctx.font = renderObject.font;
+
+  var linePositionY = renderObject.defaultPositionY;
+  linePositionY += (lineNumber === 1) ? 0 : (renderObject.fontSize * --lineNumber);
+
+  ctx.fillText(titleText, renderObject.defaultPositionX, linePositionY);
+};
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, cloudShadow, false);
   renderCloud(ctx, cloudBody, true);
 
-  ctx.fillStyle = 'black';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 240, 30);
-  ctx.fillText('Список результатов:', 230, 46);
+  renderTitleLine(ctx, 'Ура вы победили!', textStyle, 1);
+  renderTitleLine(ctx, 'Список результатов:', textStyle, 2);
 
   var findMaxTime = function () {
     var maxTime = times[0];
@@ -63,21 +79,24 @@ window.renderStatistics = function (ctx, names, times) {
   var maxTime = findMaxTime();
   var barPositionX = playerBar.startPositionX;
 
-  for (var i = 0; i < names.length; i++) {
-
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = playerBar.defaultRed;
+  var renderBar = function (renderObject, indexOfBar) {
+    if (names[indexOfBar] === 'Вы') {
+      ctx.fillStyle = renderObject.defaultRed;
     } else {
-      ctx.fillStyle = playerBar.getRandomBlue();
+      ctx.fillStyle = renderObject.getRandomBlue();
     }
 
-    var currentHeight = Math.round(times[i] * playerBar.height) / maxTime;
-    ctx.fillRect(barPositionX, playerBar.positionY, playerBar.width, currentHeight);
+    var currentHeight = Math.round(times[indexOfBar] * renderObject.height) / maxTime;
+    ctx.fillRect(barPositionX, renderObject.positionY, renderObject.width, currentHeight);
 
     ctx.fillStyle = 'black';
-    ctx.fillText(names[i], barPositionX, playerBar.positionY + 16);
-    ctx.fillText(Math.round(times[i]), barPositionX, playerBar.positionY + currentHeight - 8);
+    ctx.fillText(names[indexOfBar], barPositionX, renderObject.positionY + textStyle.fontSize);
+    ctx.fillText(Math.round(times[indexOfBar]), barPositionX, renderObject.positionY + currentHeight - (textStyle.fontSize / 2));
 
-    barPositionX += playerBar.width + playerBar.gap;
+    barPositionX += renderObject.width + renderObject.gap;
+  };
+
+  for (var i = 0; i < names.length; i++) {
+    renderBar(playerBar, i);
   }
 };
