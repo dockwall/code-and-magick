57554fd1be04af4320757da0b2b04c89bd541129
Stas <leshuksu@gmail.com>
2022-04-03 14:03:26 +0300

    Create mock arrays with wizards data

---
+++ js/setup.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var setup = document.querySelector('.setup');
+setup.classList.remove('hidden');
+
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)]'];
+var EYES = ['black', 'red', 'blue', 'yellow', 'green'];
