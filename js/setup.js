c30c0a74d821f36a54c4598ed78b07ecdb1331d1
Stas <leshuksu@gmail.com>
2022-04-16 18:58:16 +0300

    Added handlers to open/close setup popup

--- js/setup.js
+++ js/setup.js
@@ -6,7 +6,9 @@ var COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', '
 var EYES = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_COUNT = 4;
 
-var setupOpen = document.querySelector('.setup-open');
+var setupPopup = document.querySelector('.setup');
+var setupPopupOpenButton = document.querySelector('.setup-open');
+var setupPopupCloseButton = setupPopup.querySelector('.setup-close');
 
 var getRandomElement = function (array) {
   var randomIndex = Math.floor(array.length * Math.random());
@@ -58,3 +60,11 @@ var showDOMElement = function (selector) {
 
 var wizards = generateWizardsArray();
 renderWizards(wizards);
+
+setupPopupOpenButton.addEventListener('click', function () {
+  setupPopup.classList.remove('hidden');
+});
+
+setupPopupCloseButton.addEventListener('click', function () {
+  setupPopup.classList.add('hidden');
+});
