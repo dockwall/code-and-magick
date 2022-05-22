5ec9a14dfa1d6de2b21bcd5eca02d557da438395
Stas <leshuksu@gmail.com>
2022-05-22 14:45:48 +0300

    Divided into modules. Added module with mock data and key codes

---
+++ js/constants.js
@@ -0,0 +1,58 @@
+'use strict';
+
+(function () {
+  var KEY_CODES = {
+    ESC: 27,
+    ENTER: 13,
+  };
+
+  var OPTIONS = {
+    NAMES: [
+      'Иван',
+      'Хуан Себастьян',
+      'Мария', 'Кристоф',
+      'Виктор',
+      'Юлия',
+      'Люпита',
+      'Вашингтон'
+    ],
+    SURNAMES: [
+      'да Марья',
+      'Верон',
+      'Мирабелла',
+      'Вальц',
+      'Онопко',
+      'Топольницкая',
+      'Нионго',
+      'Ирвинг'
+    ],
+    COAT_COLORS: [
+      'rgb(101, 137, 164)',
+      'rgb(241, 43, 107)',
+      'rgb(146, 100, 161)',
+      'rgb(56, 159, 117)',
+      'rgb(215, 210, 55)',
+      'rgb(0, 0, 0)]'
+    ],
+    EYES: [
+      'black',
+      'red',
+      'blue',
+      'yellow',
+      'green'
+    ],
+    FIREBALL_COLORS: [
+      '#ee4830',
+      '#30a8ee',
+      '#5ce6c0',
+      '#e848d5',
+      '#e6e848',
+    ],
+    WIZARDS_COUNT: 4,
+  };
+
+  window.constants = {
+    keyCodes: KEY_CODES,
+    options: OPTIONS,
+  };
+})();
--- js/dialog.js
+++ js/dialog.js
@@ -1,66 +1,68 @@
 'use strict';
 
-var setup = document.querySelector('.setup');
-var setupDrag = setup.querySelector('.upload');
-var setupOpenButton = document.querySelector('.setup-open');
-var setupCloseButton = setup.querySelector('.setup-close');
+(function () {
+  var setup = document.querySelector('.setup');
+  var setupDrag = setup.querySelector('.upload');
+  var setupOpenButton = document.querySelector('.setup-open');
+  var setupCloseButton = setup.querySelector('.setup-close');
 
-var onOpenButtonClick = function () {
-  setupCloseButton.addEventListener('click', onCloseButtonClickResetPosition);
-};
-
-var onCloseButtonClickResetPosition = function () {
-  setup.removeAttribute('style');
-  setupCloseButton.removeEventListener('click', onCloseButtonClickResetPosition);
-};
-
-setupDrag.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
-
-  var startCoordinates = {
-    x: evt.clientX,
-    y: evt.clientY,
+  var onOpenButtonClick = function () {
+    setupCloseButton.addEventListener('click', onCloseButtonClickResetPosition);
   };
 
-  var isDragged = false;
-
-  var onSetupDragMouseMove = function (mouseMoveEvt) {
-    mouseMoveEvt.preventDefault();
-
-    var shiftCoordinates = {
-      x: startCoordinates.x - mouseMoveEvt.clientX,
-      y: startCoordinates.y - mouseMoveEvt.clientY,
-    };
-
-    startCoordinates = {
-      x: mouseMoveEvt.clientX,
-      y: mouseMoveEvt.clientY,
-    };
-
-    isDragged = true;
-
-    setup.style.top = (setup.offsetTop - shiftCoordinates.y) + 'px';
-    setup.style.left = (setup.offsetLeft - shiftCoordinates.x) + 'px';
+  var onCloseButtonClickResetPosition = function () {
+    setup.removeAttribute('style');
+    setupCloseButton.removeEventListener('click', onCloseButtonClickResetPosition);
   };
 
-  var onSetupDragMouseUp = function (mouseUpEvt) {
-    mouseUpEvt.preventDefault();
+  setupDrag.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
 
-    if (isDragged) {
-      var onSetupDragClickPreventDefault = function (clickEvt) {
-        clickEvt.preventDefault();
-        setupDrag.removeEventListener('click', onSetupDragClickPreventDefault);
+    var startCoordinates = {
+      x: evt.clientX,
+      y: evt.clientY,
+    };
+
+    var isDragged = false;
+
+    var onSetupDragMouseMove = function (mouseMoveEvt) {
+      mouseMoveEvt.preventDefault();
+
+      var shiftCoordinates = {
+        x: startCoordinates.x - mouseMoveEvt.clientX,
+        y: startCoordinates.y - mouseMoveEvt.clientY,
       };
 
-      setupDrag.addEventListener('click', onSetupDragClickPreventDefault);
-    }
+      startCoordinates = {
+        x: mouseMoveEvt.clientX,
+        y: mouseMoveEvt.clientY,
+      };
 
-    document.removeEventListener('mousemove', onSetupDragMouseMove);
-    document.removeEventListener('mouseup', onSetupDragMouseUp);
-  };
+      isDragged = true;
 
-  document.addEventListener('mousemove', onSetupDragMouseMove);
-  document.addEventListener('mouseup', onSetupDragMouseUp);
-});
+      setup.style.top = (setup.offsetTop - shiftCoordinates.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shiftCoordinates.x) + 'px';
+    };
 
-setupOpenButton.addEventListener('click', onOpenButtonClick);
+    var onSetupDragMouseUp = function (mouseUpEvt) {
+      mouseUpEvt.preventDefault();
+
+      if (isDragged) {
+        var onSetupDragClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          setupDrag.removeEventListener('click', onSetupDragClickPreventDefault);
+        };
+
+        setupDrag.addEventListener('click', onSetupDragClickPreventDefault);
+      }
+
+      document.removeEventListener('mousemove', onSetupDragMouseMove);
+      document.removeEventListener('mouseup', onSetupDragMouseUp);
+    };
+
+    document.addEventListener('mousemove', onSetupDragMouseMove);
+    document.addEventListener('mouseup', onSetupDragMouseUp);
+  });
+
+  setupOpenButton.addEventListener('click', onOpenButtonClick);
+})();
