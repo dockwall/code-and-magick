13447aa8c706c7e15213b7fc38a8ba6e1a235a58
Stas <leshuksu@gmail.com>
2022-05-25 18:00:36 +0300

    Changed error message position

--- js/error.js
+++ js/error.js
@@ -7,7 +7,7 @@
       var errorElement = document.createElement('div');
 
       errorElement.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-      errorElement.style.position = 'absolute';
+      errorElement.style.position = 'fixed';
       errorElement.style.left = 0;
       errorElement.style.right = 0;
       errorElement.style.fontSize = '30px';
